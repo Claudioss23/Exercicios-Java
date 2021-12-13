be829624370ad765d8bc6a91acd05fb0330c3641
Claudioss23 <souza.claudiof23@gmail.com>
2021-12-13 16:54:21 -0300

    Exercício Dado + Produto

---
+++ src/Mediano/Dado/Dado.java
@@ -0,0 +1,32 @@
+package Mediano.Dado;
+
+public class Dado {
+
+    private final int FACES = 6;
+    private int valorFace;
+
+    public Dado() {
+        valorFace = 1;
+    }
+
+    public int getValorFace() {
+        return valorFace;
+    }
+
+    public void setValorFace(int valorFace) {
+        if ((valorFace > 0) && (valorFace <= FACES)) {
+            this.valorFace = valorFace;
+        } else {
+            System.out.println("Valor inválido para a face do dado");
+        }
+    }
+
+    public void rolar() {
+        this.valorFace = (int) (Math.random() * FACES) + 1;
+    }
+
+    public int getQuantidadeFaces() {
+        return this.FACES;
+    }
+
+}
