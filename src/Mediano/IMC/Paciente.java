aac0a5d72a10caca2784054f0a15847159888890
Claudioss23 <souza.claudiof23@gmail.com>
2022-01-10 18:42:43 -0300

    Exercício IMC

---
+++ src/Mediano/IMC/Paciente.java
@@ -0,0 +1,45 @@
+package Mediano.IMC;
+
+public class Paciente {
+
+    private double peso, altura, imc;
+
+    public Paciente(double peso, double altura) {
+        this.peso = peso;
+        this.altura = altura;
+    }
+
+    public double calcularIMC() {
+        return peso / (altura * altura);
+    }
+
+    public void diagnostico() {
+        imc = calcularIMC();
+
+        if (imc <= 16) {
+            System.out.println("Baixo peso muito grave = IMC abaixo de 16 kg/m²");
+        }
+        if (imc >= 16 && imc <= 16.99) {
+            System.out.println("Baixo peso grave = IMC entre 16 e 16,99 kg/m²");
+        }
+        if (imc >= 17 && imc <= 18.49) {
+            System.out.println("Baixo peso = IMC entre 17 e 18,49 kg/m²");
+        }
+        if (imc >= 18.5 && imc <= 29.99) {
+            System.out.println("Peso normal = IMC entre 18,50 e 24,99 kg/m²");
+        }
+        if (imc >= 25 && imc <= 29.99) {
+            System.out.println("Sobrepeso = IMC entre 25 e 29,99 kg/m²");
+        }
+        if (imc >= 30 && imc <= 34.99) {
+            System.out.println("Obesidade grau I = IMC entre 30 e 34,99 kg/m²");
+        }
+        if (imc >= 35 && imc <= 39.99) {
+            System.out.println("Obesidade grau II = IMC entre 35 e 39,99 kg/m²");
+        }
+        if (imc >= 40) {
+            System.out.println("Obesidade grau III (obesidade mórbida) = IMC igual ou maior que 40 kg/m²");
+        }
+
+    }
+}
