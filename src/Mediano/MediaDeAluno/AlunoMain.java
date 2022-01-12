4f8b67f197f87ced4e89272a7f2624a22950543a
Claudioss23 <souza.claudiof23@gmail.com>
2022-01-12 14:18:33 -0300

    Exercício Média de Aluno

---
+++ src/Mediano/MediaDeAluno/AlunoMain.java
@@ -0,0 +1,14 @@
+package Mediano.MediaDeAluno;
+
+public class AlunoMain {
+    public static void main(String[] args) {
+        Aluno claudio = new Aluno();
+        claudio.pBim = 70;
+        claudio.sBim = 60;
+        claudio.tBim = 80;
+        claudio.qBim = 70;
+
+        System.out.println(claudio.media(claudio));
+        System.out.println(claudio.passouDeAno(claudio));
+    }
+}
