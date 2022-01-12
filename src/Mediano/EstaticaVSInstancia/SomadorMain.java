231955a15447d407140b9d1e5b5d2f598a3ce978
Claudioss23 <souza.claudiof23@gmail.com>
2022-01-12 11:54:13 -0300

    Exercício Estática X Instância

---
+++ src/Mediano/EstaticaVSInstancia/SomadorMain.java
@@ -0,0 +1,27 @@
+package Mediano.EstaticaVSInstancia;
+
+public class SomadorMain {
+    public static void main(String[] args) {
+        Somador s1 = new Somador();
+        s1.nome = "Somador 1";
+        Somador s2 = new Somador();
+        s2.nome = "Somador 2";
+        Somador s3 = new Somador();
+        s3.nome = "Somador 3";
+
+        for(int i = 0; i < 3; i ++) {
+            s1.somar();
+            s2.somar();
+            s3.somar();
+            s1.imprimir();
+            s2.imprimir();
+            s3.imprimir();
+        }
+        s2.somar();
+        s2.somar();
+
+        s1.imprimir();
+        s2.imprimir();
+        s3.imprimir();
+    }
+}
