cf52ad8c2a82248e37751c1ee5096896ea31b980
Claudioss23 <souza.claudiof23@gmail.com>
2022-01-14 13:55:46 -0300

    Teste Carro com JUnit

---
+++ src/main/java/Mediano/Carro/TestCarro.java
@@ -0,0 +1,43 @@
+package Mediano.Carro;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TestCarro {
+
+    @Test
+    public void testParado() {
+        Carro c = new Carro();
+        Assertions.assertEquals(0, c.getVelocidade());
+    }
+
+    @Test
+    public void testAcelerar() {
+        Carro c = new Carro();
+        c.potencia = 10;
+        c.acelerar();
+        Assertions.assertEquals(10, c.getVelocidade());
+    }
+
+    @Test
+    public void testFrear() {
+        Carro c = new Carro();
+        c.potencia = 10;
+        c.acelerar();
+        c.frear();
+        Assertions.assertEquals(5, c.getVelocidade());
+    }
+
+    @Test
+    public void testFrearAteZero() {
+        Carro c = new Carro();
+        c.potencia = 10;
+        c.acelerar();
+        c.frear();
+        c.frear();
+        c.frear();
+        c.frear();
+        Assertions.assertEquals(0, c.getVelocidade());
+    }
+
+}
