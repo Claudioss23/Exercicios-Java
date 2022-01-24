fe23ce39f721e2d7d8a6cd312e0f3f0d46c10be7
Claudioss23 <souza.claudiof23@gmail.com>
2022-01-24 14:45:04 -0300

    Teste Banco

---
+++ src/main/java/Mediano/Banco/ContaCorrente.java
@@ -0,0 +1,19 @@
+package Mediano.Banco;
+
+public class ContaCorrente {
+
+    int saldo;
+
+    public int sacar(int valor) {
+        if (valor > saldo) {
+            return 0;
+        } else {
+            saldo -= valor;
+            return valor;
+        }
+    }
+
+    public void depositar(int valor) {
+        saldo += valor;
+    }
+}
