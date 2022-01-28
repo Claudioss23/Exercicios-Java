2cc59970b8467f48d9d32bd6d3094941abdd8423
Claudioss23 <souza.claudiof23@gmail.com>
2022-01-28 15:46:42 -0300

    Exercício Produtos Diferentes + Testes

---
+++ src/main/java/Mediano/ProdutosDiferentes/Testes/TestCarrinhoDeCompras.java
@@ -0,0 +1,60 @@
+package Mediano.ProdutosDiferentes.Testes;
+
+import Mediano.ProdutosDiferentes.Classes.CarrinhoDeCompras;
+import Mediano.ProdutosDiferentes.Classes.Produto;
+import Mediano.ProdutosDiferentes.Classes.ProdutoComTamanho;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class TestCarrinhoDeCompras {
+
+    CarrinhoDeCompras c;
+    Produto p;
+    ProdutoComTamanho p2;
+    ProdutoComTamanho p3;
+
+    @BeforeEach
+    public void inicializaProdutoECarrinho() {
+        c = new CarrinhoDeCompras();
+        p = new Produto("Maçã", 1, 2.00);
+        p2 = new ProdutoComTamanho("Goiaba", 5, 5.00, 4);
+        p3 = new ProdutoComTamanho("Goiaba", 5, 5.00, 5);
+    }
+
+    @Test
+    public void adicionaProduto() {
+        c.adicionaProduto(p, 5);
+        c.adicionaProduto(p2, 7);
+
+        System.out.println("Quantidade de produtos no carrinho: " + c.getQuantidadeCarrinho());
+
+        Assertions.assertEquals(12, c.getQuantidadeCarrinho());
+    }
+
+    @Test
+    public void removeProduto() {
+        c.adicionaProduto(p, 5);
+        c.adicionaProduto(p2, 7);
+
+        System.out.println("Quantidade de produtos no carrinho: " + c.getQuantidadeCarrinho());
+
+        c.removeProduto(p, 3);
+
+        System.out.println("Quantidade de produtos no carrinho: " + c.getQuantidadeCarrinho());
+
+        Assertions.assertEquals(9, c.getQuantidadeCarrinho());
+    }
+
+    @Test
+    public void getPrecoTotal() {
+        c.adicionaProduto(p, 6);
+        c.adicionaProduto(p2, 10);
+        c.adicionaProduto(p3, 3);
+
+        System.out.println("Quantidade de produtos no carrinho: " + c.getQuantidadeCarrinho());
+        System.out.println("Preço total de produtos no carrinho: R$" + c.getPrecoTotal());
+
+        Assertions.assertEquals(95, c.getPrecoTotal());
+    }
+}
---
+++ src/main/java/Mediano/ProdutosDiferentes/Testes/TestProdutoComTamanho.java
@@ -0,0 +1,80 @@
+package Mediano.ProdutosDiferentes.Testes;
+
+import Mediano.ProdutosDiferentes.Classes.ProdutoComTamanho;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class TestProdutoComTamanho {
+
+    ProdutoComTamanho p;
+
+    @BeforeEach
+    public void inicializaProduto() {
+        p = new ProdutoComTamanho("Maçã", 1, 2.00, 3);
+    }
+
+    @Test
+    public void imprimirProduto() {
+        System.out.println("Produto: " + p.getNome() + "\n" +
+                "Código: " + p.getCodigo() + "\n" +
+                "Preço: R$" + p.getPreco() + "\n" +
+                "Tamanho: " + p.getTamanho() + " cm");
+        Assertions.assertEquals("Maçã", p.getNome());
+        Assertions.assertEquals(1, p.getCodigo());
+        Assertions.assertEquals(2.0, p.getPreco());
+        Assertions.assertEquals(3.0, p.getTamanho());
+    }
+
+    @Test
+    public void doisProdutosIguais() {
+        ProdutoComTamanho p2 = new ProdutoComTamanho("Maçã", 1, 2.00, 3);
+
+        System.out.println("Produto p e p2 iguais: " + p.equals(p2));
+
+        Assertions.assertEquals(p, p2);
+    }
+
+    @Test
+    public void doisProdutosDiferentes() {
+        ProdutoComTamanho p2 = new ProdutoComTamanho("Pera", 2, 3.25, 6);
+
+        System.out.println("Produto p e p2 iguais: " + p.equals(p2));
+
+        Assertions.assertNotEquals(p, p2);
+    }
+
+    @Test
+    public void doisProdutosCodigosIguais() {
+        ProdutoComTamanho p2 = new ProdutoComTamanho("Maçã", 1, 3.25, 6);
+
+        System.out.println("Produto p e p2 iguais: " + p.equals(p2));
+
+        Assertions.assertNotEquals(p, p2);
+    }
+
+    @Test
+    public void doisProdutosTamanhosIguais() {
+        ProdutoComTamanho p2 = new ProdutoComTamanho("Pera", 2, 3.25, 3);
+
+        System.out.println("Produto p e p2 iguais: " + p.equals(p2));
+
+        Assertions.assertNotEquals(p, p2);
+    }
+
+    @Test
+    public void hashcodeDeProduto() {
+        System.out.println("Hashcode de Produto: " + p.hashCode());
+        Assertions.assertEquals(1074268065, p.hashCode());
+    }
+
+    @Test
+    public void hashcodeDeDoisProdutosDiferentes() {
+        ProdutoComTamanho p2 = new ProdutoComTamanho("Graviola", 25, 4.00, 1.5);
+
+        System.out.println("Hashcode de p: " + p.hashCode());
+        System.out.println("Hashcode de p2: " + p2.hashCode());
+
+        Assertions.assertNotEquals(p.hashCode(), p2.hashCode());
+    }
+}
