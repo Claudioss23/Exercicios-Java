adb33efe567c7a2152aa83a797c5fbefe853a5c6
Claudioss23 <souza.claudiof23@gmail.com>
2022-02-03 10:50:35 -0300

    Exercício Exceções básico

---
+++ src/main/java/Básico/ExcecoesMain.java
@@ -0,0 +1,25 @@
+package Básico;
+
+public class ExcecoesMain {
+    public static void main(String[] args){
+        a(-100);
+        System.out.println("Terminando execução de main()");
+    }
+
+    public static void a(int i) {
+        System.out.println("Executando a() com " + i);
+        try {
+            b(-100);
+        } catch (Exception e) {
+            System.out.println("Tratando a exceção: " + e.getMessage());
+        }
+        System.out.println("Terminando execução de a()");
+    }
+
+    public static void b(int i) throws Exception {
+        System.out.println("Executando b() com " + i);
+        if(i > 0)
+            throw new Exception("Mensagem");
+        System.out.println("Terminando execução de b()");
+    }
+}
