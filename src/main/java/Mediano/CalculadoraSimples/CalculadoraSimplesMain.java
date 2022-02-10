981d293b241fbdcddf5b8fad3c165a8572081c55
Claudioss23 <souza.claudiof23@gmail.com>
2022-02-10 13:47:24 -0300

    Exercício de refatoração Calculadora Simples + Testes + Renomeação de "Calculadora"

--- src/main/java/Mediano/CalculadoraSimples/CalculadoraSimplesMain.java
+++ src/main/java/Mediano/CalculadoraSimples/CalculadoraSimplesMain.java
@@ -1,66 +1,76 @@
-package Básico;
+package Mediano.CalculadoraSimples;
+
+import Mediano.CalculadoraSimples.Classes.Calculos;
 
 import java.util.Scanner;
 
-public class Calculadora {
+public class CalculadoraSimplesMain {
     public static void main(String[] args) {
 
         double[] numero = new double[2];
-        double resultado;
         int escolhaCaso, escolhaOperacao = 1;
         Scanner scanner = new Scanner(System.in);
 
         while (escolhaOperacao == 1) {
-            System.out.println("Escolha a operação:\n1- Soma\n2- Subtração\n3- Multiplicação\n4- Divisão\n");
+            System.out.println("\nEscolha a operação:\n1- Soma\n2- Subtração\n3- Multiplicação\n4- Divisão\n");
+            System.out.print("> ");
             escolhaCaso = scanner.nextInt();
             switch (escolhaCaso) {
                 case 1: {
                     for (int i = 0; i <= 1; i++) {
                         System.out.println("Insira o número: ");
+                        System.out.print("> ");
                         numero[i] = scanner.nextDouble();
                     }
-                    resultado = numero[0] + numero[1];
+                    System.out.println(Calculos.soma(numero[0], numero[1]) + "\n\n");
 
-                    System.out.println("A soma é: " + resultado + "\n\n");
                     System.out.println("Escolher outra operação?\n1- Sim\n2- Não\n");
+                    System.out.print("> ");
                     escolhaOperacao = scanner.nextInt();
                     break;
                 }
                 case 2: {
                     for (int i = 0; i <= 1; i++) {
                         System.out.println("Insira o número: ");
+                        System.out.print("> ");
                         numero[i] = scanner.nextDouble();
                     }
-                    resultado = numero[0] - numero[1];
+                    System.out.println(Calculos.subtracao(numero[0], numero[1]) + "\n\n");
 
-                    System.out.println("A subtração é: " + resultado + "\n\n");
                     System.out.println("Escolher outra operação?\n1- Sim\n2- Não\n");
+                    System.out.print("> ");
                     escolhaOperacao = scanner.nextInt();
                     break;
                 }
                 case 3: {
                     for (int i = 0; i <= 1; i++) {
                         System.out.println("Insira o número: ");
+                        System.out.print("> ");
                         numero[i] = scanner.nextDouble();
                     }
-                    resultado = numero[0] * numero[1];
+                    System.out.println(Calculos.multiplicacao(numero[0], numero[1]) + "\n\n");
 
-                    System.out.println("A multiplicação é: " + resultado + "\n\n");
                     System.out.println("Escolher outra operação?\n1- Sim\n2- Não\n");
+                    System.out.print("> ");
                     escolhaOperacao = scanner.nextInt();
                     break;
                 }
                 case 4: {
                     for (int i = 0; i <= 1; i++) {
                         System.out.println("Insira o número: ");
+                        System.out.print("> ");
                         numero[i] = scanner.nextDouble();
                     }
-                    resultado = numero[0] / numero[1];
+                    System.out.println(Calculos.divisao(numero[0], numero[1]) + "\n\n");
 
-                    System.out.println("A divisão é: " + resultado + "\n\n");
                     System.out.println("Escolher outra operação?\n1- Sim\n2- Não\n");
+                    System.out.print("> ");
                     escolhaOperacao = scanner.nextInt();
                     break;
+                }
+                default: {
+                    System.out.println("Opção inválida. Terminando o programa.");
+                    System.exit(0);
                 }
             }
         }
---
+++ src/main/java/Mediano/CalculadoraSimples/Testes/TestCalculos.java
@@ -0,0 +1,37 @@
+package Mediano.CalculadoraSimples.Testes;
+
+import Mediano.CalculadoraSimples.Classes.Calculos;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TestCalculos {
+
+    @Test
+    public void testSoma() {
+        System.out.println(Calculos.soma(10, 1235));
+
+        Assertions.assertEquals(1245, Calculos.getResultado());
+    }
+
+    @Test
+    public void testSubtracao() {
+        System.out.println(Calculos.subtracao(1000, 2000));
+
+        Assertions.assertEquals(-1000, Calculos.getResultado());
+    }
+
+    @Test
+    public void testMultiplicacao() {
+        System.out.println(Calculos.multiplicacao(10, 1235));
+
+        Assertions.assertEquals(12350, Calculos.getResultado());
+    }
+
+    @Test
+    public void testDivisao() {
+        System.out.println(Calculos.divisao(10, 10));
+
+        Assertions.assertEquals(1, Calculos.getResultado());
+    }
+
+}
