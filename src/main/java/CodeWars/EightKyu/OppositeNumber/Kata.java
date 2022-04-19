2d1311107d1dda41117831ab86e7000a47ac5784
Claudioss23 <souza.claudiof23@gmail.com>
2022-04-19 10:21:32 -0300

    Exercício "Opposite Number" + Testes

---
+++ src/main/java/CodeWars/EightKyu/OppositeNumber/Kata.java
@@ -0,0 +1,8 @@
+package CodeWars.EightKyu.OppositeNumber;
+
+public class Kata {
+
+    public static int opposite(int number) {
+        return number - (number * 2);
+    }
+}
---
+++ src/main/java/CodeWars/EightKyu/OppositeNumber/TestKata.java
@@ -0,0 +1,12 @@
+package CodeWars.EightKyu.OppositeNumber;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestKata {
+
+    @Test
+    public void tests() {
+        assertEquals(-1, Kata.opposite(1));
+    }
+}
