1e4863c4a19d84ac79e95470ad8478e86833c71e
Claudioss23 <souza.claudiof23@gmail.com>
2022-04-19 10:19:25 -0300

    Exercício "SmallestInteger" + Testes

---
+++ src/main/java/CodeWars/EightKyu/SmallestInteger/SmallestIntegerFinder.java
@@ -0,0 +1,10 @@
+package CodeWars.EightKyu.SmallestInteger;
+
+import java.util.Arrays;
+
+public class SmallestIntegerFinder {
+
+    public static int findSmallestInt(int[] args) {
+        return Arrays.stream(args).min().getAsInt();
+    }
+}
