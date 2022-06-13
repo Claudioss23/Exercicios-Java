73b91aa1913137f736eaa9439bc8b4b746f1ad2e
Claudioss23 <souza.claudiof23@gmail.com>
2022-06-13 12:37:40 -0300

    Exercício "String repeat" + Testes

---
+++ src/main/java/CodeWars/EightKyu/StringRepeat/Solution.java
@@ -0,0 +1,8 @@
+package CodeWars.EightKyu.StringRepeat;
+
+public class Solution {
+
+    public static String repeatStr(final int repeat, final String string) {
+        return string.repeat(repeat);
+    }
+}
