a6c53d9b3fafb9b571b3b6f7ea2c77757195ac32
Claudioss23 <souza.claudiof23@gmail.com>
2022-06-14 10:22:35 -0300

    Exercício "Century From Year" + Testes

---
+++ src/main/java/CodeWars/EightKyu/CenturyFromYear/Solution.java
@@ -0,0 +1,8 @@
+package CodeWars.EightKyu.CenturyFromYear;
+
+public class Solution {
+
+    public static int century(int number) {
+        return (number + 99) / 100;
+    }
+}
---
+++ src/main/java/CodeWars/EightKyu/CenturyFromYear/TestSolution.java
@@ -0,0 +1,16 @@
+package CodeWars.EightKyu.CenturyFromYear;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestSolution {
+
+    @Test
+    public void FixedTests() {
+        assertEquals(18, Solution.century(1705));
+        assertEquals(19, Solution.century(1900));
+        assertEquals(17, Solution.century(1601));
+        assertEquals(20, Solution.century(2000));
+        assertEquals(1,  Solution.century(89));
+    }
+}
